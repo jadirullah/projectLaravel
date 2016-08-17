f933993115a230be200f91e836d43b4a8389f773
jadirullah@gmail <jadirullah@gmail.com>
2016-08-17 16:11:54 +0700

    cob commit pertama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Mau coba buat project laravel</div>
             </div>
         </div>
     </body>
